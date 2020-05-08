37f269719b495d365750561a692c837ecbbdb9bc
shiv kumar <shiv@riversands-MacBook-Pro.local>
2020-05-08 23:52:30 +0530

    fixed multiple sockets

--- socket.js
+++ socket.js
@@ -1,39 +1,40 @@
 
-var SOCKETS = 100;
+var SOCKETS = 10;
 
 //go
 let countGo = 0;
-
+let sockets = []
 function gosockets() {
     var ioClient = require('socket.io-client')
-    let socket = ioClient.connect('http://localhost:5007');
     for (let i = 0; i < SOCKETS; i++) {
-        ((socket) => {
-            socket.once('connect', function (args) {
-                console.log("connect go")
-                socket.emit("event:adduser", JSON.stringify({ userId: "rdwadmin@riversand.com_user", tenantId: "rdwengg-az-dev2" }))
-            });
+        let socket = ioClient.connect('http://localhost:5007');
+        socket.once('connect', function (args) {
+            console.log("connect go")
+        });
+        socket.on('event:notification', function (data) {
+            console.log("event:notification #" + i, ++countGo, data)
+        });
 
-            socket.on('event:notification', function (data) {
-                console.log("event:notification #" + i, ++countGo)
-            });
-
-            socket.on('event:message', function (data) {
-                console.log("event:message #" + i, data)
-            });
-        })(socket)
-
+        socket.on('event:message', function (data) {
+            console.log("event:message #" + i, data)
+        });
         socket.on('disconnect', function () { });
+        sockets.push(socket)
     }
-
+    setTimeout(() => {
+        sockets.forEach((socket) => {
+            console.log("emit")
+            socket.emit("event:adduser", JSON.stringify({ userId: "rdwadmin@riversand.com_user", tenantId: "rdwengg-az-dev2" }))
+        })
+    }, 100)
 }
 
 let countNode = 0
 //node
 function nodesockets() {
     var ioClient = require('socket.io-client')
-    let socket = ioClient.connect('http://localhost:5005');
     for (let i = 0; i < SOCKETS; i++) {
+        let socket = ioClient.connect('http://localhost:5005');
         ((socket) => {
             socket.once('connect', function (args) {
                 console.log("connect node")
@@ -41,11 +42,11 @@ function nodesockets() {
             });
 
             socket.on('send message', function (data) {
-                //console.log("send message #" + i, data)
+                console.log("send message #" + i, data)
             });
 
             socket.on('new message', function (data) {
-                console.log("new message #" + i, ++countNode)
+                console.log("new message #" + i, ++countNode, data)
             });
 
             socket.on('disconnect', function () { });
